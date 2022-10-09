fd533443aa6a5e2545b2663c4f10cf3449985fa4
Breno Freitas da Silva <freitasbreno174@gmail.com>
2022-10-08 23:28:03 -0300

    first commit

--- exercicio3_lista2.py
+++
@@ -1,5 +0,0 @@
-numero = float(input("Digite um número: "))
-if numero % 1 == 0:
-    print("Número Inteiro")
-else:
-    print("Numero Decimal")
--- exercicio6.py
+++
@@ -1,7 +0,0 @@
-arquivo = float(input(' Informe o tamanho do arquivo para download (em MB): '))
-link = float(input("Informe a velocidade do link de Internet (em Mbps) "))
-tempo = ((arquivo * 8) / link) / 60
-print ("Tempo aproximado de download é %.2f minutos" %tempo)
-
-
-
